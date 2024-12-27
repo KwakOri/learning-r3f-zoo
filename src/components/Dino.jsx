93b5be3075fd3a0fd822ffa002e20c3562264e65
kwakjaehun <jh5414092@icloud.com>
2024-12-27 18:00:19 +0900

    Chore: 설정 변경

--- src/components/Dino.jsx
+++ src/components/Dino.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useGLTF } from "@react-three/drei";
 import { useMemo } from "react";
 import { SkeletonUtils } from "three-stdlib";
--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Frame = ({ children }) => {
   return (
     <div
